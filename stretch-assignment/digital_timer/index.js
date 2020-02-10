0d2739ed1c16b6ea00d3ecf5caf6ccd23ea60462
Josiah Roa <josiahroa18@gmail.com>
2020-02-10 11:30:39 -0700

    Completed javascript functionality

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -10,6 +10,7 @@ class Timer{
         this.currOnes = 0;
         this.currHundreds = 0;
         this.currMs = 0;
+        this.stop = false;
     }
     updateMs(){
         if(this.currMs === 9){
@@ -29,33 +30,58 @@ class Timer{
         if(this.currOnes > 9){
             this.currOnes = 0;
             this.currTens ++;
+            for(let i=0; i<digits.length; i++){
+                digits[i].style.color = "red";
+            }
         }
     }
-    updateTimer(){
+    assignValues(){
         tens.textContent = this.currTens;
         ones.textContent = this.currOnes;
         hundreds.textContent = this.currHundreds;
         ms.textContent = this.currMs;
+    }
+    updateTimer(){
+        this.assignValues();
         this.updateMs();
         this.updateHundreds();
         this.updateOnes();
     }
     startTimer(){
         const intervalID = setInterval(() => {
-            if(this.currTens === 1){
+            if(this.currTens === 1 || this.stop){
                 this.updateTimer();
                 clearInterval(intervalID);
-                // Change color
             }else{
                 this.updateTimer();
             }
         }, 10);
     }
+    reset(){
+        this.stop = true;
+        this.currTens = 0;
+        this.currOnes = 0;
+        this.currHundreds = 0;
+        this.currMs = 0;
+        this.assignValues();
+        for(let i=0; i<digits.length; i++){
+            digits[i].style.color = "aqua";
+        }
+    }
+    start(){
+        this.stop = false;
+    }
 }
 
+let timer = new Timer();
 btnStart = document.getElementById('start');
 btnStart.addEventListener('click', () => {
-    let timer = new Timer();
+    timer.start();
     timer.startTimer();
 });
+
+btnReset = document.getElementById('reset');
+btnReset.addEventListener('click', () => {
+    timer.reset();
+})
 
