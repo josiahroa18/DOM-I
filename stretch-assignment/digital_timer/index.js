22d526abc1e01e1303c7ce974df8e4aee774ddfc
Josiah Roa <josiahroa18@gmail.com>
2020-02-10 11:00:18 -0700

    Changed method of updated timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -2,40 +2,60 @@ const tens = document.querySelector('#secondTens');
 const ones = document.querySelector('#secondOnes');
 const hundreds = document.querySelector('#msHundreds');
 const ms = document.querySelector('#msTens');
+const digits = document.querySelectorAll('.digit');
 
-let intervalId;
+class Timer{
+    constructor(){
+        this.currTens = 0;
+        this.currOnes = 0;
+        this.currHundreds = 0;
+        this.currMs = 0;
+    }
+    updateMs(){
+        if(this.currMs === 9){
+            this.currMs = 0;
+            this.currHundreds ++;
+        }else{
+            this.currMs ++;
+        }
+    }
+    updateHundreds(){
+        if(this.currHundreds > 9){
+            this.currHundreds = 0;
+            this.currOnes ++;
+        }
+    }
+    updateOnes(){
+        if(this.currOnes > 9){
+            this.currOnes = 0;
+            this.currTens ++;
+        }
+    }
+    updateTimer(){
+        tens.textContent = this.currTens;
+        ones.textContent = this.currOnes;
+        hundreds.textContent = this.currHundreds;
+        ms.textContent = this.currMs;
+        this.updateMs();
+        this.updateHundreds();
+        this.updateOnes();
+    }
+    startTimer(){
+        const intervalID = setInterval(() => {
+            if(this.currTens === 1){
+                this.updateTimer();
+                clearInterval(intervalID);
+                // Change color
+            }else{
+                this.updateTimer();
+            }
+        }, 10);
+    }
+}
 
 btnStart = document.getElementById('start');
-btnStart.addEventListener('click', function(){
-    let currTens = 0;
-    let currOnes = 0;
-    let currHundreds = 0;
-    let currMs = 0;
-    setInterval(function(){
-        tens.textContent = currTens;
-        ones.textContent = currOnes;
-        hundreds.textContent = currHundreds;
-        ms.textContent = currMs;
-        if(currMs === 9){
-            currMs = 0;
-            currHundreds ++;
-        }else{
-            currMs++;
-        }
-        if(currHundreds === 9){
-            currHundreds = 0;
-            currOnes ++;
-        }
-        if(currOnes === 9){
-            currOnes = 0;
-            currTens ++;
-        }
-
-    }, 10);
-    // console.log(intervalId);
+btnStart.addEventListener('click', () => {
+    let timer = new Timer();
+    timer.startTimer();
 });
 
-btnReset = document.getElementById('reset');
-btnStart.addEventListener('click', function(){
-    clearInterval(intervalId);
-})
